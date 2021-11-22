c76af0e349dfe4cb56596860118d0166b5eb14f5
Enock Chikobvu <sechikobvu@gmail.com>
2021-11-22 12:34:52 +0200

    blogging app

---
+++ pages/api/comments.js
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { GraphQLClient, gql } from "graphql-request"
+
+const graphqlAPI = process.env.NEXT_PUBLIC_ESCAPE_DESIGNS_ENDPOINT;
+const escapeDesignsToken = process.env.ESCAPE_DESIGNS_TOKEN;
+
+export default async function comments(req, res) {
+  const graphQLClient = new GraphQLClient((graphqlAPI), {
+    headers: {
+      authorization: `Bearer ${escapeDesignsToken}`,
+    },
+  });
+
+  const query = gql`
+  mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+    createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
+  }
+ `;
+
+  try {
+    const result = await graphQLClient.request(query, req.body);
+
+    return res.status(200).send(result);
+  } catch (error) {
+    console.log('start here==============')
+    console.log(error)
+    return res.status(500).send(error);
+  }
+
+
+}
